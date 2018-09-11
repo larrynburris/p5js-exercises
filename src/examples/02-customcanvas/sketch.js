71fe2887447b49522800c64e344a4d669a4fcdc0
larrynburris <larry.n.burris@gmail.com>
2018-09-10 21:54:04 -0400

    Custom canvas example

---
+++ src/examples/02-customcanvas/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  var c1 = createCanvas(600, 400);
+  c1.parent('canvas1');
+}
+
+function draw() {
+  var x = 250;
+  var y = 250;
+  var w = 280;
+  var h = 280;
+  ellipse(x, y, w, h);
+}
